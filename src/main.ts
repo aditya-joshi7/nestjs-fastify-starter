dbbd31d4965f04ccf5d617a059426efa5deb6bb1
Aditya Joshi <joshiaditya73@yahoo.com>
2023-09-24 11:34:01 +0530

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,23 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './module/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(3000, '0.0.0.0', () => {
+  const config = new DocumentBuilder()
+    .setTitle('NestJS BoilerPlate')
+    .setDescription('APIs for project')
+    .setVersion('1.0')
+    .addTag('boilerplate')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT, '0.0.0.0', () => {
     console.log(`Application started on port ${process.env.PORT}`);
   });
 }
